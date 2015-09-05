8455fe2b8472e282ae96fd16ff3cabdc72387c8f
Barry Cohen <barrycohen7@gmail.com>
2015-09-05 12:35:20 +0100

    dotfiles removed

---
+++ php/LoversBlog/posts.php
@@ -0,0 +1,15 @@
+<?php include 'includes/header.php' ?>
+  <div class="blog-post">
+	<h2 class="blog-post-title">International PHP Conference 2015</h2>
+	<p class="blog-post-meta">January 1, 2015 by <a href="#">Mark</a></p>
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut diam aliquet, blandit urna sit amet, molestie arcu. Sed venenatis justo ut ipsum ultricies laoreet. Suspendisse potenti. Ut ac nunc at augue aliquam mollis. Curabitur fringilla iaculis orci, sit amet consectetur quam pellentesque at. Aliquam efficitur nunc massa, ac accumsan risus lobortis non. Morbi tellus nisi, gravida vitae dolor a, dignissim aliquam nulla. Nunc at sem mattis, lacinia lectus dignissim, egestas tortor.</p>
+	<a class="readmore" href="post.php?id=1">Read More</a>
+  </div><!-- /.blog-post -->
+
+  <div class="blog-post">
+	<h2 class="blog-post-title">PHP 5.6.0beta4 Released</h2>
+	<p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut diam aliquet, blandit urna sit amet, molestie arcu. Sed venenatis justo ut ipsum ultricies laoreet. Suspendisse potenti. Ut ac nunc at augue aliquam mollis. Curabitur fringilla iaculis orci, sit amet consectetur quam pellentesque at. Aliquam efficitur nunc massa, ac accumsan risus lobortis non. Morbi tellus nisi, gravida vitae dolor a, dignissim aliquam nulla. Nunc at sem mattis, lacinia lectus dignissim, egestas tortor.</p>
+	<a class="readmore" href="post.php?id=1">Read More</a>
+  </div><!-- /.blog-post -->
+<?php include 'includes/footer.php' ?>        
