48722db210a026c4b29f42f3a5bf3f8145a5955f
Barry Cohen <barrycohen7@gmail.com>
2015-09-05 12:33:11 +0100

    wiping dotfiles

--- php/ShoutIt/database.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//connect to MySQL
-$con = mysqli_connect("localhost", "root", "root", "shout-it");
-
-//Test connection
-if(mysqli_connect_errno()){
-	echo 'Failed to connect to MySQL: '.mysqli_connect_error();
-}
-?>
